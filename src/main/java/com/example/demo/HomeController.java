15176711060d7cb92b32d488560f606e5b07257c
chenyuhjiuan <emilymarinovich@hotmail.com>
2019-09-26 17:03:12 -0400

    Java Bootcamp Blog

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){return "home";}
+
+    @RequestMapping("/bootcamp")
+    public String bootcamp(){return "bootcamp";}
+
+    @RequestMapping("/author")
+    public String author(){return "author";}
+
+    @RequestMapping("/polymorphism")
+    public String polymorphism(){return "polymorphism";}
+
+
+}
